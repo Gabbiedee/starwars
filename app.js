465d4056039c29c70f7d6148aa22462af554a5f2
Gabbiedee <akingbojudolly@gmail.com>
2023-02-08 20:27:49 +0100

    formatted HTML page

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const cards = document.querySelectorAll('.card')
 const endpoint = 'https://swapi.dev/api/films';
+const loading = document.getElementById('loading');
+const movieCards = document.getElementById('movieCards');
 
 
 async function fetchpost() {
     const response = await fetch (endpoint);
     const data = await response.json();
-    
-    const results = data.results
 
+    return data.results;
+    
+}
+
+async function loadMovie() {
+    movieCards.style.display = 'none';
+
+    const results = await fetchpost()
     
     for (let i = 0; i < results.length; i++) {
         const cardBody = cards[i].querySelector('.card-body');
@@ -17,21 +25,17 @@ async function fetchpost() {
                                 <h6>{Episodes: ${results[i].episode_id}} </h6>
                               </div>
                               <div style="font-size:12px;">
-                              <span style="color:red;;">Release date:</span> ${results[i].release_date} <br>
-                              <span style="color:red;">Director:</span> ${results[i].director}
+                              <span class="text-danger">Release date:</span> ${results[i].release_date} <br>
+                              <span class="text-danger"">Director:</span> ${results[i].director}
                               </div>
                               `
 
-        // change.style.color = "red";
-        // console.log(results[i])
-        // console.log(cards[i])
       }
-     
-    
+      loading.remove();
+      movieCards.style.display = 'flex';
 }
 
-
-fetchpost();
+ loadMovie();
     
 
   
@@ -41,17 +45,5 @@ fetchpost();
 
 
 
-// window.onload = async function fetchpost(){
-    
-// }
-
-// window.addEventListener("load", fetchpost)
-
-//     async function fetchpost() {
-//         const response = await fetch (endpoint);
-//         const data = await response.json();
-        
-//         console.log(data)
-//     }
     
 
