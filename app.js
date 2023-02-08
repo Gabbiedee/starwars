bff0795e6d25a1647d4793987b882f20b68f4eb0
Gabbiedee <akingbojudolly@gmail.com>
2023-02-08 17:51:03 +0100

    Complete version2

--- app.js
+++ app.js
@@ -1,57 +1,53 @@
-const cards = document.querySelectorAll('.card')
+const movieCards = document.getElementById('movieCards');
+const loading = document.getElementById('loading');
 const endpoint = 'https://swapi.dev/api/films';
+const images = [
+    './images/img1.png',
+    './images/img2.png',
+    './images/img3.png',
+    './images/img4.png',
+    './images/img5.png',
+    './images/img6.png'
+]
+const movieCard = (result, imgSrc)=>{
+    const {title, episode_id, release_date, director} = result;
+return (
+    `<div class="col-md-4">
+    <div class="card" style="width: 18rem;">
+    <img src="${imgSrc}" class="card-img-top" alt="...">
+    <div class="card-body ">
+    <div>
+         <h5>${title}
+        </h5>
+         <h6>{Episodes: ${episode_id}} </h6>
+        </div>
+        <div style="font-size:12px;">
+        <span style="color:red;">Release date:</span> ${release_date} <br>
+        <span style="color:red;">Director:</span> ${director}
+        </div>
+    </div>
+  </div>
+</div>`
+)
 
-
-async function fetchpost() {
-    const response = await fetch (endpoint);
-    const data = await response.json();
-    
-    const results = data.results
-
-    
-    for (let i = 0; i < results.length; i++) {
-        const cardBody = cards[i].querySelector('.card-body');
-        cardBody.innerHTML = `<div>
-                                <h5>${results[i].title}
-                                </h5>
-                                <h6>{Episodes: ${results[i].episode_id}} </h6>
-                              </div>
-                              <div style="font-size:12px;">
-                              <span style="color:red;;">Release date:</span> ${results[i].release_date} <br>
-                              <span style="color:red;">Director:</span> ${results[i].director}
-                              </div>
-                              `
-
-        // change.style.color = "red";
-        // console.log(results[i])
-        // console.log(cards[i])
-      }
-     
-    
 }
 
+const fetchpost= async () => {
+    const response = await fetch (endpoint);
+    const data = await response.json();
 
-fetchpost();
-    
+    return data.results
+}
 
-  
-    
+const loadMovie = async () => {
+    const results = await fetchpost();
+    let cards = "";
+    for(let i = 0; i < results.length; i++){
+        cards += movieCard(results[i], images[i])
 
-   
+    }
+    loading.remove();
+    movieCards.innerHTML = cards
+}
 
-
-
-// window.onload = async function fetchpost(){
-    
-// }
-
-// window.addEventListener("load", fetchpost)
-
-//     async function fetchpost() {
-//         const response = await fetch (endpoint);
-//         const data = await response.json();
-        
-//         console.log(data)
-//     }
-    
-
+loadMovie();
